5b7672687d8e3c6b767a98e4a05fe06bf3b65755
Abhishek Rajpurohit <53573825+Abhiraj9757@users.noreply.github.com>
2021-05-15 20:11:41 +0530

    Add files via upload
    Web content Mining

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿  <%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication23._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+      <style>
+          table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+          </style>
+    <div class="jumbotron">
+       
+       <p>Scrap your Data No need to copy paste an individual Line Everytime.</p>
+            <asp:Button  Style="background-color: #eeeeee" ID="Button1" runat="server" OnClick="Button1_Click" Text="Search Using your Web Link" />
+
+        <asp:Button Style="background-color: #eeeeee" ID="Button2" runat="server"  OnClick="Button2_Click" Text="Save Data for youself to work more on.... " />
+        </div>      
+    
+  
+</asp:Content>
---
+++ Site.Master.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace WebApplication23
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = web.Load(TextBox1.Text);
+
+                var Articles = doc.DocumentNode.SelectNodes("//*[@class ='article-single']");
+               
+                foreach (var article in Articles)
+                {
+                    var header = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class='article-header']" + "\n").InnerText);
+
+                    var description = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class='article-copy']").InnerText);
+                    Response.Write("<Table>");
+
+                    Response.Write("<td>");
+                    Response.Write("Name - " + header);
+                    Response.Write("<br />");
+                    Response.Write(" Description - " + description);
+                    Response.Write("<tr />");
+                    Response.Write("<td />");
+                    Response.Write("</Table>");
+                }
+               
+
+            }
+            catch (Exception ex) { Response.Write(ex.Message); }
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(TextBox1.Text);
+
+            var Articles = doc.DocumentNode.SelectNodes("//*[@class ='article-single']");
+
+            string excelName = "data";
+
+            Response.ClearContent();
+            //Response.AddHeader("content-disposition", "attachment;filename=" + excelName + ".xls");
+            //Response.AddHeader("Content-Type", "application/vnd.ms-excel");
+
+            Response.ContentType = "application/vnd.ms-excel";
+
+            Response.AppendHeader("content-disposition", "attachment; filename="+excelName+".xls");
+
+            //Header for table records  
+            //  
+            Response.Write("Header");
+            Response.Write("\t");
+            Response.Write("Description");
+            Response.Write("\t");
+
+            foreach (var article in Articles)
+            {
+                var header = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class='article-header']" + "\n").InnerText);
+
+                var description = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class='article-copy']").InnerText);
+
+                Response.Write(header);
+                Response.Write("\t");
+                Response.Write(description);
+                Response.Write("\t");
+                Response.Write("\n");
+
+
+            }
+            Response.End();
+
+        }
+    }
+}
